93b678b71d850181893e389c2ac26eff0d51d689
Augusto <auguzsto@gmail.com>
2022-09-24 14:56:31 -0300

    global items + class classes

--- js/Rpg.js
+++ js/Rpg.js
@@ -1,7 +1,7 @@
-let baseItems = {
+let basedb = {
     "Items": [
         {
-           "": ""
+           "":""
         },
         {
             "id": "1", "name": "Sword of King", "type": "Sword", "level": "18", "description": "King Arthas's First Sword", "rarity": "Common", "attr": "+2 Agility", "durability": "100", "duration": "None", "itemType": "Weapon"
@@ -9,6 +9,18 @@ let baseItems = {
         {
             "id": "2", "name": "Holt Frut", "type": "Food", "level": "34", "description": "Made by Vandora's portal guardians, regenerates 10% health and gives +20 strength", "rarity": "Rare", "attr": "+21 Stronger", "durability": "100", "duration": "30min", "itemType": "Food"
         },
+    ],
+
+    "Classes": [
+        {
+            "":""
+        },
+        {
+            "id": "1", "name": "Paladin", "races": "Humans"
+        },
+        {
+            "id": "2", "name": "Warrior", "races": "Humans, Gnomes, Elfs, Worgens, Globins, Undead"
+        }
     ]
 }
 
@@ -22,38 +34,26 @@ class Items {
         this.attr = attr;
         this.durability = durability;
         this.duration = duration;
-        this.itemType = itemType
+        this.itemType = itemType;
     }
 }
 
-class Food extends Items {
-    constructor(name, type, level, description, rarity, attr, duration, itemType) {
-        super(name, type, level, description, rarity, attr, duration, itemType)
-    }
-    
-    Info() {
-        console.log("Name: "+this.name+ " - Type: "+this.type+ " - Level: "+this.level+ " - Description: "+this.description+ " - Rarity: "+this.rarity+ " - Attributes: "+this.attr+" - Duration: "+this.durability);
+class Classes {
+    constructor(name, races) {
+        this.name = name;
+        this.races = races;
     }
 }
 
-class Weapons extends Items {
-    constructor(name, type, level, description, rarity, attr, durability, itemType) {
-      super(name, type, level, description, rarity, attr, durability, itemType)
+class Paladin extends Classes {
+    constructor(name, races, spells, requeriments){
+        super(name, races)
+        this.spells = spells;
+        this.requeriments = requeriments;
     }
-
-    Info() {
-        console.log("Name: "+this.name+ " - Type: "+this.type+ " - Level: "+this.level+ " - Description: "+this.description+ " - Rarity: "+this.rarity+ " - Attributes: "+this.attr+" - Durability: "+this.durability);
-    }
-
-    Repair() {
-        if(this.durability == 0){
-            console.log(this.name+  " quebrou! Faça o reparo para poder utilizar.");
-        } else if(this.durability < 45) {
-            console.log(this.name+ " precisa ser reparada.");
-        } else {
-            console.log("A durabilidade da "+this.name+" está em " +this.durability+ ". Não é necessário reparo, por enquanto...");
+        spells() {
+            //
         }
-    }
 }
 
 function getItems(n) { //Search item from console.
